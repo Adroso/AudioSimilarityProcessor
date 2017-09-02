5a0fb4633519f9a987c9093b6468e1634407c649
Adroso <adrian.lapico@my.jcu.edu.au>
2017-09-02 16:28:56 +1000

    Minor Code Cleanup, and finish

--- main.py
+++ main.py
@@ -6,49 +6,51 @@ Scope: Be able to classify samples of music into genre
 Future Implementation: Be able to infer intended emotional response from music to make recommendations
 """
 
-#Imports
-import numpy as np
-import matplotlib.pyplot as plt
+# Imports
 import matplotlib.style as ms
+
 ms.use('seaborn-muted')
-import IPython.display
 import librosa
 import librosa.display
 
-#filefinding
+# filefinding
 from tkinter.filedialog import askopenfilename
-
 
 AUDIO_PATH_1 = 'Trainer_MusicType1/rock2.mp3'
 AUDIO_PATH_2 = 'Trainer_MusicType2/tech1.mp3'
-AUDIO_PATH_5 ="TestData/coldplay.mp3"
+AUDIO_PATH_5 = "TestData/coldplay.mp3"
+
 
 def main():
     print("Starting Program..")
 
-    #if want to find audio on system
-    #filename = askopenfilename()
-    #y, sr = audioLoad(filename)
+    # if want to find audio on system
+    # filename = askopenfilename()
+    # y, sr = audioLoad(filename)
 
-    y, sr = audioLoad(AUDIO_PATH_1)
-    findBPM(y, sr, AUDIO_PATH_1)
+    y, sr = audio_load(AUDIO_PATH_1)
+    find_bpm(y, sr, AUDIO_PATH_1)
 
 
-def audioLoad(audioPath):
-    #TODO Load every audio file from a specified directory and save the outputs
+def audio_load(audio_path):
+    # TODO Load every audio file from a specified directory and save the outputs
     try:
-        print("Loading..... ", audioPath)
-        y, sr = librosa.load(audioPath)
-        print("Audio Path: ", audioPath, " Loaded")
+        print("Loading..... ", audio_path)
+        y, sr = librosa.load(audio_path)
+        print("Audio Path: ", audio_path, " Loaded")
         return y, sr
     except:
         print("Failed To Load Audio File")
         print("Closing")
 
-def findBPM(y, sr, audioPath):
-    tempo, beats = librosa.beat.beat_track(y, sr)
-    print("{} is {} BPM".format(audioPath.split('/', 1)[-1], tempo))
 
-def audioClassify():
+def find_bpm(y, sr, audio_path):
+    tempo, beats = librosa.beat.beat_track(y, sr)
+    print("{} is {} BPM".format(audio_path.split('/', 1)[-1], tempo))
+
+
+def audio_classify():
     pass
+
+
 main()
