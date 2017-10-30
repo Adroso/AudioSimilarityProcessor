0353e1bd85eddf5b89ed6da8a133ef0d6ed17eee
Adroso <adrian.lapico@my.jcu.edu.au>
2017-10-30 15:18:14 +1000

    added more informative output

--- MainSecond.py
+++ MainSecond.py
@@ -15,17 +15,19 @@ import librosa.display
 import os
 import numpy as np
 
+# note position 0 in songs array corresponds to position 2 in songtypes
+SONGS = ['Trainer_MusicType1/rock6.mp3', 'Trainer_MusicType1/rock4.mp3', 'Trainer_MusicType2/tech3.mp3',
+         'Trainer_MusicType2/tech5.mp3']
+SONGTYPES = [0, 0, 1, 1]
+SONGTYPES = ['rock', 'rock', 'techno', 'techno']
+
+TESTSONGS = ['Trainer_MusicType1/rock3.mp3', 'Trainer_MusicType1/rock5.mp3', 'Trainer_MusicType2/tech1.mp3',
+             'Trainer_MusicType2/tech2.mp3']
+
+
+# TESTSONGSTYPES = ['rock', 'techno']
 
 def main():
-    # note position 0 in songs array corresponds to position 2 in songtypes
-    SONGS = ['Trainer_MusicType1/rock6.mp3', 'Trainer_MusicType1/rock4.mp3', 'Trainer_MusicType2/tech3.mp3',
-             'Trainer_MusicType2/tech5.mp3']
-    SONGTYPES = [0, 0, 1, 1]
-    SONGTYPES = ['rock', 'rock', 'techno', 'techno']
-
-    TESTSONGS = ['Trainer_MusicType1/rock3.mp3', 'Trainer_MusicType1/rock5.mp3', 'Trainer_MusicType2/tech1.mp3',
-                 'Trainer_MusicType2/tech2.mp3']
-    # TESTSONGSTYPES = ['rock', 'techno']
 
     raw_waveforms = audio_load(SONGS)
     raw_features = extract_features(raw_waveforms)
@@ -84,12 +86,12 @@ def audio_classifyer(features, lables, testingFeatures):
     # kmeans.labels_ = classifyer_lables
 
     results = kmeans.predict(testing_data)
-    print(results)
 
+    song = 0
+    for i in results:
+        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], i))
+        song += 1
 
-
-    # print(averaged_mfccs)
-    # print(lables)
 
 
 
