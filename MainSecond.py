073b047f1b3584bb171358fb43fa2991c634afee
Adroso <adrian.lapico@my.jcu.edu.au>
2017-11-06 21:32:27 +1000

    modified output lables to be more scalable

--- MainSecond.py
+++ MainSecond.py
@@ -16,29 +16,23 @@ graphviz (for decsion tree visualisation)
 
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn import tree
+import numpy as np
 import librosa
 import librosa.display
-import numpy as np
-import graphviz
-
 
 # note position 0 in songs array corresponds to position 2 in songtypes
 SONGS = ['Trainer_MusicType1/rock5.mp3', 'Trainer_MusicType1/rock4.mp3', 'Trainer_MusicType1/rock3.mp3',
          'Trainer_MusicType1/rock2.mp3', 'Trainer_MusicType2/tech3.mp3',
          'Trainer_MusicType2/tech5.mp3', 'Trainer_MusicType2/tech1.mp3', 'Trainer_MusicType2/tech4.mp3']
 SONGTYPES = [0, 0, 0, 0, 1, 1, 1, 1]
-# SONGTYPES = ['rock', 'rock', 'techno', 'techno']
 
 TESTSONGS = ['Trainer_MusicType3/techLuke.mp3', 'Trainer_MusicType3/technoAdri.mp3', 'Trainer_MusicType3/rockLuke2.m4a',
-             'Trainer_MusicType3/unknownLuke.mp3', 'Trainer_MusicType3/unknownLuke.mp3',
+             'Trainer_MusicType3/unknownLuke.mp3', 'Trainer_MusicType3/rockLuke.mp3',
              'Trainer_MusicType3/orcestral_type3.mp3', 'Trainer_MusicType3/technoJap.mp3',
              'Trainer_MusicType3/The Chainsmokers - Dont Let Me Down (Illenium Remix).mp3']
 
+SONGLABLES = ["Rock", "Techno"]  # 0 will match up to type 0 in lables above etc
 
-# TESTSONGS = ['Trainer_MusicType1/rock3.mp3', 'Trainer_MusicType1/rock5.mp3', 'Trainer_MusicType2/tech4.mp3',
-#              'Trainer_MusicType2/tech2.mp3', 'Trainer_MusicType3/orcestral_type3.mp3']
-
-# TESTSONGSTYPES = ['rock', 'techno']
 
 def main():
 
@@ -105,7 +99,7 @@ def extract_features(raw_sounds):
                                                       sr=samrate).T, axis=0)
         tonnetz = np.average(raw_tonnetz)
 
-        print("chroma  ", chroma, "  | mel  ", mel, " | Contrast  ", contrast, " | tonnetz  ", tonnetz)
+        print("chroma: ", chroma, "\nmel: ", mel, "\nContrast: ", contrast, "\ntonnetz: ", tonnetz)
 
         audioFeatures.append([mfccs, bpm, chroma, mel, contrast, tonnetz])
 
@@ -114,7 +108,7 @@ def extract_features(raw_sounds):
 
 
 def audio_classifyer(features, lables, testingFeatures):
-    print("KMEANS CLUSTERING")
+    print("\nKMEANS CLUSTERING")
     classifyer_data = np.asanyarray(features)
     classifyer_lables = np.asanyarray(lables)
     testing_data = np.asanyarray(testingFeatures)
@@ -131,17 +125,12 @@ def audio_classifyer(features, lables, testingFeatures):
     print("KMEANS")
     song = 0
     for i in results:
-        if i == 1:
-            txtLab = "Techno/Electronic"
-        else:
-            txtLab = "Rock"
-
-        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], txtLab))
+        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], SONGLABLES[i]))
         song += 1
 
 
 def decision_tree_classifyer(features, lables, testingFeatures):
-    print("DECISION TREE CLASSIFYER")
+    print("\n\nDECISION TREE CLASSIFYER")
     classifyer_data = np.asanyarray(features)
     classifyer_lables = np.asanyarray(lables)
     testing_data = np.asanyarray(testingFeatures)
@@ -149,18 +138,13 @@ def decision_tree_classifyer(features, lables, testingFeatures):
     dt_clf = tree.DecisionTreeClassifier().fit(classifyer_data, classifyer_lables)
     results = dt_clf.predict(testing_data)
 
-    dec_tree_data = tree.export_graphviz(dt_clf, out_file=None)
-    graph = graphviz.Source(dec_tree_data)
-    graph.render("iris")
+    # dec_tree_data = tree.export_graphviz(dt_clf, out_file=None)
+    # graph = graphviz.Source(dec_tree_data)
+    # graph.render("iris")
 
     song = 0
     for i in results:
-        if i == 1:
-            txtLab = "Techno/Electronic"
-        else:
-            txtLab = "Rock"
-
-        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], txtLab))
+        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], SONGLABLES[i]))
         song += 1
 
 
@@ -187,7 +171,7 @@ def mini_batch(features, lables, testingFeatures):
         else:
             txtLab = "Rock"
 
-        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], txtLab))
+        print("{} song has been clustered into song: type {}.".format(TESTSONGS[song], SONGLABLES[i]))
         song += 1
 
 
